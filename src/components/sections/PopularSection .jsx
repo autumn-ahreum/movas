6a01c2c9b0591b4f50a2b5075322ae5870da50de
Ahreum Han <beautumn43@gmail.com>
2024-02-18 17:46:21 -0800

    Connecting api for popular movie

--- src/components/sections/PopularSection .jsx
+++ src/components/sections/PopularSection .jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React , { useState, useEffect }from 'react'
 import { dummy } from '../../cardDummy';
 import Card from '../elements/Card';
+
+// API  
+// URL = base_url + actual_url + API_key
+let API_key = "&api_key=337d4aa999639b97c6ca769ff17079b1";
+const base_url = "https://api.themoviedb.org/3";
+let url = base_url + "/movie/popular?language=en-US&page=1" + API_key;
 
 // Related to swiper only below
 // import Swiper core and required modules
@@ -16,26 +22,39 @@ import 'swiper/css/scrollbar';
 
 
 export default () => {
+  const [ movieData, setData ] = useState([]);
+  const [url_set, setUrl] = useState(url);
+
+  useEffect ( ()=> {
+    fetch(url_set).then(res=>res.json()).then(data => {
+      // console.log(data.results);
+      setData(data.results);
+    }, [url_set])
+  }
+  )
+
+
   return (
+    <>
+    <div class= "cards-section popular" >
+    <h2>Popular</h2>
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={2}
       slidesPerView={2}
       navigation
       scrollbar={{ draggable: true }}
-
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      {/* <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide> */}
-      
-      <div class= "cards-section popular" >
       {
-        dummy.results.map((item) => {
-          return (
+        (movieData.length === 0)? (
+          <SwiperSlide>
+            <p>Not Found : movie data fetch error</p>
+          </SwiperSlide>
+        )  : 
+        movieData.map(item  => (
+
           <SwiperSlide key={item.id}>
             <Card 
               title = {item.title}
@@ -45,11 +64,11 @@ export default () => {
               release_date = {item.release_date}
             />
           </SwiperSlide>
-          )
-        })
-      }
-    </div>
+        ))
+      }    
     </Swiper>
+    </div>
+    </>
   );
 };
 
