107d052b3b089ce398ec30542a2bf978ae23903f
autumn-ahreum <hanjiro77@gmail.com>
2025-03-06 10:53:07 -0800

    edit vite cofig

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",  // 상대 경로 설정
+  base: "/movas/",  // 상대 경로 설정
   build: {
     outDir: "movas",  // 빌드 파일을 movas 폴더로 저장
     assetsDir: "assets",  // CSS, JS 파일을 movas/assets 폴더에 저장
